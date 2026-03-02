728a9384a86ab77a76bd0e7fa6529798e93617c4
YaelAlc <3.aramirezalcantarajorgeyael@gmail.com>
2026-03-02 01:27:35 -0600

    Se creo el archivo ReportesDao.java

---
+++ app/src/main/java/yael/alcantara/uptreport/db/dao/ReportesDao.java
@@ -0,0 +1,24 @@
+package yael.alcantara.uptreport.db.dao;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import yael.alcantara.uptreport.db.Reportes;
+
+@Dao
+public interface ReportesDao {
+
+    @Insert
+    void insertarReportes(Reportes reportes);
+
+    @Update
+    void actualizarReportes(Reportes reportes);
+
+    @Delete
+    void borrarReportes(Reportes reportes);
+
+
+}
